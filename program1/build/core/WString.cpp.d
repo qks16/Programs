db83a2dc34eb6a045fefad5f66c3d32c12c2afb1
qks16 <qks16@txstate.edu>
2026-02-02 12:36:52 -0600

    Delete program1 directory

--- program1/build/core/WString.cpp.d
+++
@@ -1,3 +0,0 @@
-C:\Users\Jeremiah\Programs\program1\build\core\WString.cpp.o: \
- C:\Users\Jeremiah\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino\WString.cpp \
- C:\Users\Jeremiah\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino\WString.h
--- program1/build/core/abi.cpp.d
+++
@@ -1,2 +0,0 @@
-C:\Users\Jeremiah\Programs\program1\build\core\abi.cpp.o: \
- C:\Users\Jeremiah\AppData\Local\Arduino15\packages\arduino\hardware\avr\1.8.6\cores\arduino\abi.cpp
